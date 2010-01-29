c0f1ddc9bba42ff454b2aca3c1d490c328b66a4f
zethon <zethon@78416e20-fba9-11de-a9e7-e7de9bbfa21f>
2010-01-29 04:43:54 +0000

    test update

--- trunk/src/dotTOC/src/ConsoleAim/ConsoleAim/ConsoleCommands.cs
+++ trunk/src/dotTOC/src/ConsoleAim/ConsoleAim/ConsoleCommands.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
 namespace ConsoleAim
 {
     class ConsoleCommands
     {
+
+        public void ExecuteCommand(string strCommand)
+        {
+
+        }
     }
 }
