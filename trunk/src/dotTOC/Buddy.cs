4cb4c5cccbe036bfbeea5f348498d9a225de75e0
aclaure <aclaure@78416e20-fba9-11de-a9e7-e7de9bbfa21f>
2010-01-31 18:56:51 +0000

    initial revision

---
+++ trunk/src/dotTOC/Buddy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dotTOC
+{
+    public enum BuddyStatus
+    {
+        Offline,
+        Online,
+        Idle
+    }
+
+    class Buddy
+    {
+        private string _strName = string.Empty;
+        public string Name
+        {
+            get { return _strName; }
+        }
+
+        private BuddyStatus _status = BuddyStatus.Offline;
+        public BuddyStatus Status
+        {
+            get { return _status; }
+        }
+    }
+}
