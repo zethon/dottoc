7c943490df75593dbf929a273eddacd045a0f7ae
aclaure <aclaure@78416e20-fba9-11de-a9e7-e7de9bbfa21f>
2010-01-31 13:16:07 +0000

    cleaned up some code

--- src/dotTOC/TOCUser.cs
+++ src/dotTOC/TOCUser.cs
@@ -4,16 +4,25 @@ using System.Text;
 
 namespace dotTOC
 {
-	public enum NameFormat { Raw,Normalized }
-	public enum PasswordFormat { Raw,Roasted }
+    public enum NameFormat { Raw, Normalized }
+    public enum PasswordFormat { Raw, Roasted }
 
-	/// <summary>
-	/// class for the screen name signed on using the TOC class
-	/// </summary>
+    /// <summary>
+    /// class for the screen name signed on using the TOC class
+    /// </summary>
     public class TOCUser
     {
-        private string m_strName;
-        private string m_strPW;
+        private string _strName;
+        public string UserName
+        {
+            get { return _strName; }
+        }
+
+        private string _strPW;
+        public string Password
+        {
+            get { return _strPW; }
+        }
 
         public TOCUser()
         {
@@ -21,8 +30,8 @@ namespace dotTOC
 
         public TOCUser(string strName, string strPW)
         {
-            m_strName = strName;
-            m_strPW = strPW;
+            _strName = strName;
+            _strPW = strPW;
         }
 
         public string GetName()
@@ -32,10 +41,14 @@ namespace dotTOC
 
         public string GetName(NameFormat nt)
         {
-            if (nt == NameFormat.Normalized)
-                return Normalize(m_strName);
+            string strRetVal = _strName;
 
-            return m_strName;
+            if (nt == NameFormat.Normalized)
+            {
+                strRetVal = Normalize(strRetVal);
+            }
+
+            return strRetVal;
         }
 
         public string GetPassword()
@@ -45,10 +58,46 @@ namespace dotTOC
 
         public string GetPassword(PasswordFormat pt)
         {
-            if (pt == PasswordFormat.Roasted)
-                return RoastedString(m_strPW);
+            string strRetVal = _strPW;
 
-            return m_strPW;
+            if (pt == PasswordFormat.Roasted)
+            {
+                strRetVal = RoastedString(strRetVal);
+            }
+
+            return strRetVal;
+        }
+
+        /// <summary>
+        /// Returns a normalized version of the string, will concate the string to 16 chars
+        /// if necessary
+        /// </summary>
+        public static string Normalize(string strScreenName)
+        {
+            string strName = strScreenName;
+            strName = System.Text.RegularExpressions.Regex.Replace(strName," ", "");
+            strName = strName.ToLower();
+
+            if (strName.Length > 16)
+            {
+                strName = strName.Remove(16, strName.Length - 16);
+            }
+            
+            return strName;
+        }
+
+        public static string RoastedString(string strOrig)
+        {
+            const string roaster = "Tic/TocTic/TocTic/TocTic/Toc";
+            string retStr = "0x";
+
+            for (int i = 0; i < strOrig.Length; i++)
+            {
+                retStr += string.Format("{0:x2}", strOrig[i] ^ roaster[i]);
+            }
+
+            return retStr;
         }
     }
+
 }
