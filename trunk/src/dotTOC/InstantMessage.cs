0d1db5e4924f98b8f58c0b34d16b75dee6edbaee
aclaure <aclaure@78416e20-fba9-11de-a9e7-e7de9bbfa21f>
2010-03-26 01:38:07 +0000

    added InstantMessage class

---
+++ trunk/src/dotTOC/InstantMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dotTOC
+{
+    public class InstantMessage
+    {
+        public string From;
+        public string To;
+        public string Message;
+        public bool Auto = false;
+    }
+}
