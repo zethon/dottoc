200aa6706259e16cd3085c0a11ad47af82e87124
aclaure <aclaure@78416e20-fba9-11de-a9e7-e7de9bbfa21f>
2010-04-04 21:07:31 +0000

    added version incrementation on Release builds

---
+++ src/dotTOC/verinc.pl
@@ -0,0 +1,22 @@
+#!/usr/perl/bin
+use strict;
+use Data::Dumper;
+
+open FILE, 'AssemblyInfo.cs' or die $!; 
+my @lines = <FILE>;
+close FILE;
+
+open FILE, '>AssemblyInfo.cs' or die $!; 
+foreach my $line (@lines)
+{
+	if ($line =~ /Version\(\"\d+\.\d+.\d+.(\d+)\"\)\]/)
+	{
+		my $ver = $1;
+		$ver++;
+
+		$line =~ s/Version\(\"(\d+)\.(\d+).(\d+).(\d+)\"\)\]/Version\(\"$1\.$2.$3.$ver\"\)\]/;
+	}
+	
+	print FILE $line;
+}
+close FILE;
